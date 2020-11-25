e0a3adda6dbb2d2a379d8185de59ae45d830fb37
Carly Jugler <carjug@gmail.com>
2020-11-25 15:04:50 -0700

    WIP rrb sample app and their models

--- hello_world/models.py
+++ hello_world/models.py
@@ -9,6 +9,9 @@ class Person(models.Model):
     last_name = models.CharField(max_length=100)
     street_address = models.CharField(max_length=120)
     address_unit = models.CharField(max_length=10)
+    address_city = models.CharField(max_length=30)
+    address_state = models.CharField(max_length=2, choices=state choices list somewhere)
+    address_zip_code = models.CharField(max_length=14)
     date_of_birth = models.DateField()
     phone_number = models.CharField(max_length=11)
     # social_security = unknown how to store this yet
