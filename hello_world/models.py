8fa91e3b14a5e2d58c298b2b9579517d3e705471
Carly Jugler <carjug@gmail.com>
2020-11-25 14:53:31 -0700

    WIP rrb sample app and their models

--- hello_world/models.py
+++ hello_world/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    street_address = models.CharField(max_length=120)
+    address_unit = models.CharField(max_length=10)
+    date_of_birth = models.DateField()
+    phone_number = models.CharField(max_length=11)
+    # social_security = unknown how to store this yet
+
+
+class Application(models.Model):
+    class ApplicationType(models.TextChoices):
+        SICKNESS = 'SI', _('Sickness')
+        UNKNOWN = 'UN', _('Unknown')
+
+    person = models.ForeignKey(
+        Person,
+        models.SET_NULL,
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT
+    )
+    type = models.CharField(
+        max_length=20,
+        choices=ApplicationType.choices
+    )
+
+
+
